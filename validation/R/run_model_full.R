3548bc9ee0e0c07b3030a18c5caa6cd5c41ab489
Johannes Kruisselbrink <jwkruisselbrink@gmail.com>
2025-05-18 15:44:33 +0200

    Cleanup R validation script

--- validation/R/run_model_full.R
+++ validation/R/run_model_full.R
@@ -1,4 +1,4 @@
-# This r script is used as a standalone program to test the PBPK dll
+# This R script is used as a standalone program to test the PBK dll
 rm(list=ls()) 
 
 library(deSolve)
@@ -9,44 +9,54 @@ R.version
 dllName <- "PBPK_generic_model_v6"
 dllPath <- paste("validation/R/", dllName, sep="")
 resultsPath <- "validation/R/results"
+parametrisationsFilename <- "validation/R/EuroMix-KineticModels-Cosmos to be completed_6.csv"
+idModelInstance <- "CosmosClothianidin6"
 
-# Get parameter values from file
-data <- read.csv("validation/R/EuroMix-KineticModels-Cosmos to be completed_6.csv", header = TRUE,sep=",")
-nominal <- read.csv("validation/R/parameters_6.csv", header = TRUE,sep=",")$parameters
-data <- subset(data, idModelInstance == "CosmosClothianidin6")
+###############################################
+# Helper functions
+###############################################
 
-readInputs <- function() {
+loadParametrisation <- function(
+  filename,
+  idModelInstance
+) {
   # Set parameters
-  readParameters = data$Parameter
-  Inputs <- rep(c(0),length(nominal))
+  parameterIndexes <- read.csv("validation/R/parameters_6.csv", header = TRUE,sep=",")$parameters
 
-  for (ii in 1:length(nominal)){
+  # Get parameter values from file
+  data <- read.csv(filename, header = TRUE,sep=",")
+  data <- subset(data, idModelInstance == idModelInstance)
+
+  readParameters = data$Parameter
+  params <- rep(c(0),length(parameterIndexes))
+  for (ii in 1:length(parameterIndexes)){
     for(jj in 1:length(readParameters)){
-      if (toString(nominal[ii]) == toString(readParameters[jj])){
-        Inputs[ii] <- data$Value[jj]
+      if (toString(parameterIndexes[ii]) == toString(readParameters[jj])){
+        params[ii] <- data$Value[jj]
       }
     }
   }
 
-  names(Inputs) <- nominal
+  names(params) <- parameterIndexes
 
   # Initialize correlations, normally done within MCRA
-  Inputs["log_aPoor"] <- log(Inputs["PCPoor"]/Inputs["PCFat"])
-  Inputs["log_aRich"] <- log(Inputs["PCRich"]/Inputs["PCFat"])
-  Inputs["log_aLiver"] <- log(Inputs["PCLiver"]/Inputs["PCFat"])
-  Inputs["log_aSkin"] <- log(Inputs["PCSkin"]/Inputs["PCFat"])
-  Inputs["log_aSkin_sc"] <- log(Inputs["PCSkin_sc"]/Inputs["PCFat"])
-  Inputs["ResampledPCFat"] <- Inputs["PCFat"]
-  Inputs["log_PCFat"] <- log(Inputs["PCFat"])
+  params["log_aPoor"] <- log(params["PCPoor"]/params["PCFat"])
+  params["log_aRich"] <- log(params["PCRich"]/params["PCFat"])
+  params["log_aLiver"] <- log(params["PCLiver"]/params["PCFat"])
+  params["log_aSkin"] <- log(params["PCSkin"]/params["PCFat"])
+  params["log_aSkin_sc"] <- log(params["PCSkin_sc"]/params["PCFat"])
+  params["ResampledPCFat"] <- params["PCFat"]
+  params["log_PCFat"] <- log(params["PCFat"])
 
-  for (ii in 1:length(nominal)){
-    print(paste(ii, ': ', nominal[ii],'=', Inputs[ii]))
+  for (ii in 1:length(parameterIndexes)){
+    print(paste(ii, ': ', parameterIndexes[ii],'=', params[ii]))
   }
-  return(Inputs)
+  return(params)
 }
-Inputs <- readInputs()
 
-#######################################################################
+###############################################
+# Set dosing
+###############################################
 
 # set number of simulated days
 days <- 1
@@ -57,7 +67,7 @@ doseOral <- 0
 doseDermal <- 0
 doseInhalation <- 0
 
-events <- c(0: (days*steps-1))
+events <- c(0:(days*steps-1))
 
 oralEvent <- c(1,2)
 oralDosing <- rep(0,steps)
@@ -78,8 +88,12 @@ inhalationDosing <- rep(c(inhalationDosing),days)
 allDoses <- list(cbind(events, oralDosing), cbind(events, dermalDosing), cbind(events, inhalationDosing))
 eventLevels <- sort(unique(c(events[oralDosing>0], events[dermalDosing>0],events[inhalationDosing>0])))
 
+###############################################
+# Load model
+###############################################
+
 # Unload dll when loaded
-result <- tryCatch({
+tryCatch({
   dyn.unload(paste(dllName, .Platform$dynlib.ext, sep=""))
 }, error = function(err) {}, finally = {})
 
@@ -87,34 +101,41 @@ result <- tryCatch({
 system(paste('R CMD SHLIB "', dllPath, '.c"', sep=""))
 dyn.load(paste(dllPath, .Platform$dynlib.ext, sep=""))
 
-# Output names
-Outputs <- c("CTotal","CVen","CArt","CFat","CPoor","CRich","CLiver","CSkin_u","CSkin_e","CSkin_sc_u","CSkin_sc_e")
-
 # Set resolution and times
 resolution <- 3
 times  <- seq(from=0, to= days*steps*60, by=resolution)/60
 
+###############################################
 # Run for MM
-Inputs <- readInputs()
-Inputs$Michaelis <- 1
-Inputs$Km <- 1
-Inputs <- .C("getParms",  as.double(Inputs), out= double(length(Inputs)), as.integer(length(Inputs)), PACKAGE = dllName)$out
-Y <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0)
-outnew <- ode(y=Y, times=times, func="derivs", parms=Inputs, dllname = dllName, 
-              initfunc = "initmod", initforc = "initforc", forcings = allDoses, events = list(func="event", time=eventLevels), 
-              nout = length(Outputs), outnames = Outputs )
+###############################################
+
+params <- loadParametrisation(parametrisationsFilename, idModelInstance)
+params['Michaelis'] <- 1
+params['Km'] <- 1
+params <- .C("getParms",  as.double(params), out= double(length(params)), as.integer(length(params)), PACKAGE = dllName)$out
+
+# Output names
+outputNames <- c("CTotal","CVen","CArt","CFat","CPoor","CRich","CLiver","CSkin_u","CSkin_e","CSkin_sc_u","CSkin_sc_e")
+
+y <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0)
+outnew <- ode(y=y, times=times, func="derivs", parms=params, dllname=dllName, 
+              initfunc="initmod", initforc="initforc", forcings=allDoses, events=list(func="event", time=eventLevels), 
+              nout=length(outputNames), outnames=outputNames)
 
 write.csv(outnew, paste(resultsPath, "/euromix_r_results_MM.csv", sep=""))
 
+###############################################
 # Run for MA
-Inputs <- readInputs()
-Inputs$Michaelis <- 0
-Inputs$Km <- 1
-Inputs <- .C("getParms",  as.double(Inputs), out= double(length(Inputs)), as.integer(length(Inputs)), PACKAGE = dllName)$out
-Y <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0)
-outnew <- ode(y=Y, times=times, func="derivs", parms=Inputs, dllname = dllName,
-              initfunc = "initmod", initforc = "initforc", forcings = allDoses, events = list(func="event", time=eventLevels),
-              nout = length(Outputs), outnames = Outputs )
+###############################################
+
+params <- loadParametrisation(parametrisationsFilename, idModelInstance)
+params['Michaelis'] <- 0
+params['Km'] <- 1
+params <- .C("getParms",  as.double(params), out= double(length(params)), as.integer(length(params)), PACKAGE = dllName)$out
+y <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0)
+outnew <- ode(y=y, times=times, func="derivs", parms=params, dllname = dllName,
+              initfunc="initmod", initforc="initforc", forcings=allDoses, events=list(func="event", time=eventLevels),
+              nout=length(outputNames), outnames=outputNames )
 
 write.csv(outnew, paste(resultsPath, "/euromix_r_results_MA.csv", sep=""))
 
